7597817a64781c741dd0781e499ab417ce055c55
jioniy <jw010208@gmail.com>
2023-03-12 04:50:29 +0900

    [ADD] #2 Management Web App using only Servlet(make html by servlet)

---
+++ servlet/servlet/src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,48 @@
+package hello.servlet.web.servlet;
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        System.out.println("MemberSaveServlet.service");
+
+        /* request parameter로 받은 데이터로 Member 객체 생성*/
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        Member member = new Member(username, age);
+        System.out.println("member = " + member);
+
+        /* MemberRepository를 이용해 Member 객체 저장*/
+        memberRepository.save(member);
+
+        /* Member 객체를 이용해서 결과 화면용 HTML을 동적으로 만들어서 응답 */
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter w = response.getWriter();
+        w.write("<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                " <li>id="+member.getId()+"</li>\n" +
+                " <li>username="+member.getUsername()+"</li>\n" +
+                " <li>age="+member.getAge()+"</li>\n" +
+                "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+}
